4fbd45d47b50c315af662eb0e2ffdfcb544852ce
Minhajul Islam <137941240+Minhajulcse@users.noreply.github.com>
2023-06-30 01:16:47 +0600

    Unary operator

---
+++ program11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+
+{
+    int x = 10;
+
+    int y = x++;
+
+    cout <<x <<endl;
+
+
+    cout <<y <<endl;
+
+
+
+
+    getch();
+}
