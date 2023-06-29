e628e96f3a6cc177fe3011707ec9bb65df164efa
Minhajul Islam <137941240+Minhajulcse@users.noreply.github.com>
2023-06-30 01:02:09 +0600

    Assignment operator

---
+++ program10.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+
+{
+    int x = 20;
+    int y = 5;
+
+    x += y;    // x = x+y = 25
+
+    cout << x <<endl;
+
+    x -=y ;    // x = x-y = 20
+    cout << x <<endl;
+
+
+    x *=y ;     // x = x*y = 100
+
+    cout << x <<endl;
+
+
+
+
+    getch();
+}
