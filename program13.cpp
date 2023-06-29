9a238326ab5d03556fc34e081709ae5fcdb51247
Minhajul Islam <137941240+Minhajulcse@users.noreply.github.com>
2023-06-30 01:45:33 +0600

    Special operator

---
+++ program13.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    int a,b,sum;
+
+    sum = (a= 10 , b=20, sum=a+b);
+    cout << sum <<endl;
+
+
+
+
+    /*
+    some special operator is,
+    comma(,)
+    pointer (*)
+    sizeof()
+    */
+
+
+
+
+    getch();
+}
