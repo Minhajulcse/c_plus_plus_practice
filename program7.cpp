f8dafbd9c2245c477bfb17aaba7be3a6b410c6a1
Minhajul Islam <137941240+Minhajulcse@users.noreply.github.com>
2023-06-30 00:33:05 +0600

    Area of triangle

---
+++ program7.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+
+{
+
+    double b,h,area;
+
+    cout << "Enter Base : ";
+    cin >> b ;
+    cout <<endl;
+
+    cout << "Enter Height: " ;
+    cin>> h;
+    cout <<endl;
+
+
+    area = (double) 1/2 * b * h ;
+
+    cout << "Area of triangle : " << area;
+    cout <<endl;
+
+
+
+
+
+    getch();
+}
+
