906048f8e12ede3b7b30fa32b257f2564690803d
Minhajul Islam <137941240+Minhajulcse@users.noreply.github.com>
2023-06-30 23:09:15 +0600

    Even or Odd number

---
+++ program17.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+
+
+    int num;
+    cout << "Enter any integer number : ";
+    cin >> num;
+
+    if(num%2==0)
+    {
+        cout << "Even";
+    }
+    else
+    {
+        cout << "Odd";
+    }
+
+
+
+    getch();
+}
