c92cc8c0c4ffbea0e8794c61a53ee0c8121eaa16
Minhajul Islam <137941240+Minhajulcse@users.noreply.github.com>
2023-06-30 23:16:35 +0600

    pass or fail

---
+++ program19.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+
+
+    int num;
+    cout << "Enter your number : ";
+    cin >> num;
+
+    if(num>=33)
+    {
+        cout << "Pass";
+    }
+
+
+    else
+    {
+        cout << "Fail";
+    }
+
+
+
+    getch();
+}
+
