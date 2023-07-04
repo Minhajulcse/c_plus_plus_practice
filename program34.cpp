675630b5710258275503d7cae3ecd9e28c8ed6e5
Minhajul Islam <137941240+Minhajulcse@users.noreply.github.com>
2023-07-04 07:10:27 +0600

    do while loop

---
+++ program34.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int i=1;
+    do
+    {
+
+        cout<< "Minhajul" << endl;
+        i++;
+    }while(i<=10);
+
+
+
+    getch();
+}
+
