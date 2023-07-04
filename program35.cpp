1c16a7c0da2506ae9dd233a6f0c149ca21490123
Minhajul Islam <137941240+Minhajulcse@users.noreply.github.com>
2023-07-04 21:38:56 +0600

    break statemebt

---
+++ program35.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    for(int i=1; i<=50; i++)
+    {
+        if(i==7)
+        {
+            break;
+        }
+        cout <<i<<endl;
+    }
+
+
+
+    getch();
+}
